3a644d689bfa7d61655e984550dfdfb5ff19d9ad
Andrew Walker <andrew@almostawake.com>
2024-04-11 09:22:50 +1000

    drawer not opening, style overridden

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.getElementById('openDrawer').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "openDrawer"});
+    });
+  });
+  
